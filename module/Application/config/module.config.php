a2cc1e0c92bb684a646966712800fa396261facd
trondal <trondal@cry>
2013-08-27 16:25:35 +0200

    Set default layout

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -73,6 +73,7 @@ return array(
         'not_found_template' => 'error/404',
         'exception_template' => 'error/index',
         'template_map' => array(
+            'layout/layout' => __DIR__ . '/../view/layout/application.phtml',
             'application/layout' => __DIR__ . '/../view/layout/application.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'error/404' => __DIR__ . '/../view/error/404.phtml',
