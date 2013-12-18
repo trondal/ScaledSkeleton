9ca8a4cc52e3e0041d2401f27a169872dbccfc8e
Trond Albinussen <trond.albinussen@gmail.com>
2013-12-18 10:39:34 +0100

    System wide error templates/layout. Could not use EpdModuleLayouts as it could not handle subnamespaces.

--- config/autoload/app.development.php
+++ config/autoload/app.development.php
@@ -6,6 +6,13 @@ return array(
     'view_manager' => array(
         'display_not_found_reason' => true,
         'display_exceptions' => true,
+        'exception_template' => 'error',
+        'not_found_template' => '404',
+        'template_map' => array(
+            'error' => __DIR__ . '/../../module/Application/view/error/error.phtml', // <-- system wide error template
+            '404' => __DIR__ . '/../../module/Application/view/error/404.phtml',  // <-- system wide 404 template
+            'layout/layout' => __DIR__ . '/../../module/Application/view/layout/application.phtml' // <-- error layout
+        )
     ),
     'doctrine' => array(
         'configuration' => array(
--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -50,14 +50,9 @@ return array(
     ),
     'view_manager' => array(
         'doctype' => 'HTML5',
-        'not_found_template' => 'error/404',
-        'exception_template' => 'error/index',
         'template_map' => array(
-            'layout/layout' => __DIR__ . '/../view/layout/application.phtml',
             'application/layout' => __DIR__ . '/../view/layout/application.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404' => __DIR__ . '/../view/error/404.phtml',
-            'error/index' => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
